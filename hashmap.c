17340bbec72e87b0a15a4d81ea79eeee014615f5
Gabriel Saiago <gabriel@beloc.al>
2023-08-19 18:09:17 -0300

    Add: access

--- hashmap.c
+++ hashmap.c
@@ -34,7 +34,7 @@ void  *mallocTable(unsigned int size, int (*compareFunc)(void *), void (*freeFun
   return (table + sizeof(t_tableMetaData));
 }
 
-void  freeTable(t_tableMetaData *table) // tem que colocar o parametro extra pra ser uma função que vai limpar os valores das chaves
+void  freeTable(t_tableMetaData *table)
 {
   t_tableEntry    *contentRef;
   unsigned int    index;
@@ -57,6 +57,23 @@ void  freeTable(t_tableMetaData *table) // tem que colocar o parametro extra pra
   free(table);
 }
 
+static unsigned int  hashFunction(char *key)
+{
+  unsigned int  i;
+  unsigned int keyHash;
+
+  if (!key)
+    return (0);
+  i = 0;
+  keyHash = 0;
+  while (key[i])
+  {
+    keyHash += key[i] * i;
+    i++;
+  }
+  return (keyHash);
+}
+
 void  *insertOnTable(t_tableMetaData *table, char *key, void *value)
 {
   int           i;
@@ -66,21 +83,34 @@ void  *insertOnTable(t_tableMetaData *table, char *key, void *value)
 
   if (!key)
     return (NULL);
-  i = 0;
-  while (key[i])
-  {
-    keyHash += key[i] * i;
-    i++;
-  }
-  tableIndex = keyHash % *(int *)((void *)table - sizeof(t_tableMetaData));
+  keyHash = hashFunction(key);
+  tableIndex = keyHash % *(unsigned int *)((void *)table - sizeof(t_tableMetaData));
   entryPosition = (t_tableEntry *)table + tableIndex;
   entryPosition->keyName = ft_strdup(key, 0);
   entryPosition->keyHash = keyHash;
   entryPosition->value = value;
+  printf("---- Insertion Start ----\n");
+  printf("KeyHash = %d\n", entryPosition->keyHash);
   printf("tableIndex: %d\n", tableIndex);
   printf("KeyName = %s\n", entryPosition->keyName);
-  printf("KeyHash = %d\n", entryPosition->keyHash);
+  printf("KeyValue = %s\n", entryPosition->value);
+  printf("---- Insertion End ----\n");
   return (&table[tableIndex]);
+}
+
+void  *accessTable(void *table, char *key)
+{
+  unsigned int  keyHash;
+  unsigned int  tableIndex;
+
+  if (!key)
+    return (NULL);
+  keyHash = hashFunction(key);
+  tableIndex = keyHash % *(unsigned int *)((void *)table - sizeof(t_tableMetaData));
+  if (((t_tableEntry *)(table))[tableIndex].keyHash == keyHash)
+    return (((t_tableEntry *)(table))[tableIndex].value);
+  else 
+    return (NULL);
 }
 
 int testF(void *ptr)
@@ -92,14 +122,22 @@ int testF(void *ptr)
 int main(void)
 {
   void  *table = mallocTable(10, testF, free);
-  void  *content = malloc(10);
+  void  *content = ft_strdup("string 0", 0);
+  char  *access;
+
   if (!table)
     return (0);
   insertOnTable(table, "prop0", content);
-  content = malloc(10);
+  content = ft_strdup("string 1", 0);
   insertOnTable(table, "prop1", content);
-  content = malloc(10);
+  content = ft_strdup("string 2", 0);
   insertOnTable(table, "prop2", content);
+  access = accessTable(table, "prop0");
+  printf("Access na prop0: %s\n", access);
+  access = accessTable(table, "prop1");
+  printf("Access na prop1: %s\n", access);
+  access = accessTable(table, "prop2");
+  printf("Access na prop2: %s\n", access);
   freeTable(table);
   return (0);
 }
